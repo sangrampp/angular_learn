7011a86f95ede3cffeb8e1a1f958883d81a4c5ca
sangrampp <sangrampp@gmail.com>
2015-10-31 13:48:47 +0530

    Added service working

--- SPA/app.js
+++ SPA/app.js
@@ -12,10 +12,30 @@ angularjsWeatherApp.config(function($routeProvider){
     })
 });
 
-angularjsWeatherApp.controller('cityController', ['$scope', function($scope){
+angularjsWeatherApp.service('weatherservice', function(){
+    this.cityName = '';
+});
+
+angularjsWeatherApp.controller('cityController', ['$scope', 'weatherservice',  function($scope, weatherservice){
+    $scope.cityName = '';
     
+    $scope.$watch('cityName', function(){
+        console.log($scope.cityName);
+        weatherservice.cityName = $scope.cityName;
+    });
 }]);
 
-angularjsWeatherApp.controller('weatherController', ['$scope', function($scope){
+angularjsWeatherApp.controller('weatherController', ['$scope', '$resource', 'weatherservice',  function($scope, $resource, weatherservice){
+    $scope.cityName = weatherservice.cityName;
+    
+    $scope.weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast/daily', { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+    
+    $scope.weatherresult = $scope.weatherAPI.get({
+        q: $scope.cityName,
+        cnt: 2,
+        appid: 'bd82977b86bf27fb59a04b61b657fb6f'
+    });
+    
+    console.log($scope.weatherresult);
     
 }]);
