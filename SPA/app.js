05c3c91b248d840c2879095cdbb5fb26c396e554
sangrampp <sangrampp@gmail.com>
2015-10-31 15:33:54 +0530

    Added daily-temperature directive

--- SPA/app.js
+++ SPA/app.js
@@ -20,6 +20,13 @@ angularjsWeatherApp.service('weatherservice', function(){
     this.cityName = '';
 });
 
+angularjsWeatherApp.directive('dailyTemperature', function(){
+    return {
+        templateUrl: 'directives/dailytemperature.html',
+        replace: true
+    }    
+});
+
 angularjsWeatherApp.controller('cityController', ['$scope', 'weatherservice',  function($scope, weatherservice){
     $scope.cityName = '';
     
@@ -29,7 +36,7 @@ angularjsWeatherApp.controller('cityController', ['$scope', 'weatherservice',  f
     });
 }]);
 
-angularjsWeatherApp.controller('weatherController', ['$scope', '$resource', '$routeParams', 'weatherservice',  function($scope, $resource, $routeParams, weatherservice){
+angularjsWeatherApp.controller('weatherController', ['$scope', '$resource', '$routeParams', '$filter', 'weatherservice',  function($scope, $resource, $routeParams, $filter,  weatherservice){
     $scope.cityName = weatherservice.cityName;
     $scope.days = $routeParams.numdays || 2;
     $scope.weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast/daily', { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
@@ -39,6 +46,14 @@ angularjsWeatherApp.controller('weatherController', ['$scope', '$resource', '$ro
         cnt: $scope.days,
         appid: 'bd82977b86bf27fb59a04b61b657fb6f'
     });
+    
+    $scope.convertToDate = function(dt){
+        return $filter('date')(dt*1000, 'MMM dd, yyyy');
+    }
+    
+    $scope.convertToCelsius = function(temp){
+        return Math.round(temp - 273);
+    }
     
     console.log($scope.weatherresult);
     
